b42dce2f04b04cafc3637c74bcd2d8777a4d7ce1
sproutleaf <mq435@nyu.edu>
2022-10-24 22:19:08 -0400

    Working version for class presentation

--- midterm-roulette/midterm-roulette.ino
+++ midterm-roulette/midterm-roulette.ino
@@ -1,32 +1,37 @@
 #include "Shifter.h"
 #include "Bitmap.h"
+#include "ConwaysWayOfLife.h"
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 // Variables for the shift registers
-#define DATA_PIN 4 // DATA PIN
-#define LATCH_PIN 5 // LATCH PIN
-#define CLOCK_PIN 6 // CLOCK PIN
+#define DATA_PIN 4   // DATA PIN
+#define LATCH_PIN 5  // LATCH PIN
+#define CLOCK_PIN 6  // CLOCK PIN
 
 #define NUM_REGISTERS 3
-Shifter shifter(DATA_PIN, LATCH_PIN, CLOCK_PIN, NUM_REGISTERS); 
+Shifter shifter(DATA_PIN, LATCH_PIN, CLOCK_PIN, NUM_REGISTERS);
 
 // Variables for the OLED display
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-
 #define OLED_RESET 4
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define LOGO_HEIGHT   64
-#define LOGO_WIDTH    128
-
-// Other initializations
+// Button Initializations
 #define BUTTON_PIN 7
-int buttonState;  // Either 0 for unpressed or 1 for pressed
-int delayMs = 100;  // Delay between LED lights when the ring is spinning
+int buttonState;   // Either 0 for unpressed or 1 for pressed
+
+// Initializations for the lights and counters
+int delayMs = 50;  // Delay between LED lights when the ring is spinning
+int gifDelayMs = 150;
+unsigned long previousTime = millis();
+unsigned long ledPreviousTime = millis();
+unsigned long lastExitTime = millis();
+int i = 0;
+int j = 0;
 int randomInc;  // Increment to delay once button is pressed
 bool slowDownActivated = false;
 
@@ -37,24 +42,42 @@ void setup() {
   Serial.begin(9600);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ;  // Don't proceed, loop forever
   }
-
-  drawStartPumpkin(); // Draw the pumpkin for the beginning of the game
 }
 
 void loop() {
-  for (int i = 0; i < NUM_REGISTERS * 8; i++) {
+  drawAnimation();
+  blinkLights();
+}
+
+void drawAnimation() {
+  unsigned long currentTime = millis();
+  if (currentTime - previousTime > gifDelayMs) {
+    previousTime = currentTime;
+    display.clearDisplay();
+    display.drawBitmap(0, 0, *(way_of_life_frames[j]), 128, 64, 1);
+    display.display();
+    j++;
+    if (j == 30) {
+      j = 0;
+    }
+  }
+}
+
+void blinkLights() {
+  unsigned long ledCurrentTime = millis();
+  if (ledCurrentTime - ledPreviousTime > delayMs) {
+    Serial.println(delayMs);
+    ledPreviousTime = ledCurrentTime;
     shifter.clear();
     shifter.setAll(HIGH);
     shifter.setPin(i, LOW);
     shifter.write();
-    delay(delayMs);
-    Serial.println(delayMs);
     if (slowDownActivated) {
-      Serial.println("Slowdown activated!");
       randomInc = random(30, 70);
       delayMs += randomInc;
 
@@ -63,9 +86,14 @@ void loop() {
       // 2) Display the corresponding image / text on OLED;
       // 3) Reset slow down activated state.
       if (delayMs >= 1000) {
+        if (i == 0 || i == 13) {
+          drawPumpkin();          
+        } else {
+          drawPumpkin();
+          // displayResult(i);
+        }
         buttonState = digitalRead(BUTTON_PIN);
         while (buttonState == LOW) {
-          Serial.println("Button not pressed!");
           buttonState = digitalRead(BUTTON_PIN);
         }
         delayMs = 100;
@@ -75,32 +103,41 @@ void loop() {
         // this way, even if someone is pressing the button for a long time, there
         // will not be mixup with the next iteration.
         while (buttonState == HIGH) {
-          Serial.println("Button pressed!");
           buttonState = digitalRead(BUTTON_PIN);
         }
+        lastExitTime = millis();
       }
     } else {
-      // We only want to read the button state when slow down has not been triggered.
-      buttonState = digitalRead(BUTTON_PIN);
-      Serial.println("Button state");
-      Serial.println(buttonState);
-      // Once a button press is detected, we want to start the process of slowing down
-      // the light movement. For this button, LOW = not pressed; HIGH = pressed.
-      if (buttonState == HIGH) {
-        Serial.println("Button pressed!?");
-        slowDownActivated = true;
+      unsigned long buttonCurrentTime = millis();
+      if (buttonCurrentTime - lastExitTime > delayMs) {
+        // We only want to read the button state when slow down has not been triggered.
+        buttonState = digitalRead(BUTTON_PIN);
+        // Once a button press is detected, we want to start the process of slowing down
+        // the light movement. For this button, LOW = not pressed; HIGH = pressed.
+        if (buttonState == HIGH) {
+          slowDownActivated = true;
+        }
       }
+    }
+    i++;
+    if (i == 24) {
+      i = 0;
     }
   }
 }
 
-void drawStartPumpkin() {
+void drawPumpkin() {
   display.clearDisplay();
-
-  display.drawBitmap(
-    (display.width()  - LOGO_WIDTH ) / 2,
-    (display.height() - LOGO_HEIGHT) / 2,
-    pumpkin, LOGO_WIDTH, LOGO_HEIGHT, 1);
+  display.drawBitmap(0, 0, myBitmap, 128, 64, 1);
   display.display();
-  delay(1000);
+}
+
+void displayResult(int i) {
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(50, 20);
+  display.println(i + 1);
+  display.invertDisplay(1);
+  display.display();
 }
